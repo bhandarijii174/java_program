b0c07c5b85b6ee6a843958d1d969d9c7ca3b54d3
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:42:08 +0530

    Create bool.java

---
+++ bool.java
@@ -0,0 +1,7 @@
+public class bool {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 45;
+    System.out.println(x > y); //it will give false because 5 is not greator than 45.
+  }
+}
