e69e9cc1813e660b4fc7fc65a67cb60162e32e8b
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:44:29 +0530

    Create continue.java

---
+++ continue.java
@@ -0,0 +1,10 @@
+public class continuee {
+  public static void main(String[] args) {
+    for (int i = 0; i < 20; i++) {
+      if (i % 2 == 0) {//only odd numbers will be printed.
+        continue;
+      }
+      System.out.println(i);
+    }  
+  }
+}
