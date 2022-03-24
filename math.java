8abc39807f0a398d5ddee2ffc0f70233e475909b
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:47:19 +0530

    Create math.java

---
+++ math.java
@@ -0,0 +1,9 @@
+
+public class math {
+  public static void main(String[] args) {
+    System.out.println(Math.max(58, 50));  
+	System.out.println(Math.min(4, 10));  
+	System.out.println(Math.sqrt(144)); 
+	System.out.println(Math.random());
+  }
+}
