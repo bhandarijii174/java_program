0fac9e595e405acdf678d4051892ba8f4fd53046
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:43:18 +0530

    Create comments.java

---
+++ comments.java
@@ -0,0 +1,7 @@
+public class comments {
+  public static void main(String[] args) {
+    System.out.println("we have used comments in tbis program.the comments can't be seen in the output of the program");//this is a single line comment.
+    System.out.println("comments are very important");/*this is a multiline
+	comment*/
+  }
+}
