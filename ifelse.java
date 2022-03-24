4ff65df06389bf20e6b6d6e3a598c9b42950015d
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:46:40 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+  public static void main(String[] args) {
+    int a=2;
+	int b=4;
+    if (a < b) {
+      System.out.println("2 is less than 4");
+    } else if (b < a) {
+      System.out.println("4 is less than 2");
+    }  else {
+      System.out.println("4 and 2 are equal");
+    }
+  }
+}
