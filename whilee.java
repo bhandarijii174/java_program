da82d171a5c91ae6677d28c86ac422e371f0642c
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:51:00 +0530

    Create whilee.java

---
+++ whilee.java
@@ -0,0 +1,10 @@
+public class whilee {
+  public static void main(String[] args) {
+    int a=4;
+    int i = 1;
+    while (i < 11) {
+      System.out.println(a*i);//it gives out the table of 4.
+      i++;
+    }  
+  }
+}
