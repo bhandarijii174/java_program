98847a1d445b989461fc0b579fb3e2a1697a7b25
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:49:55 +0530

    Create typecast.java

---
+++ typecast.java
@@ -0,0 +1,9 @@
+public class typecast {
+  public static void main(String[] args) {
+    int integer= 14;
+    double DOUBLE = integer; //integer is converted to double.
+
+    System.out.println(integer);
+    System.out.println(DOUBLE);
+  }
+}
