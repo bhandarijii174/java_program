91ac2e86877c1fdc44d708c8aeccc31e042ada3a
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:45:12 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes {
+  public static void main(String[] args) {
+    int myNum = 85;               // integer (whole number)
+    float myFloatNum = 7.89f;    // floating point number
+    char myLetter = 'N';         // character
+    boolean myBool = false;       // boolean
+    String myText = "Hello nikhil";     // String    
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
+© 2022 GitHub, Inc.
