e10d28b32c1e965f0043bbcc89d9ec15725ca7db
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:47:54 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,12 @@
+public class operators {
+  public static void main(String[] args) {
+    int x = 100 + 50;
+    System.out.println(x);
+	int y = 10;
+    y += 5;
+    System.out.println(y);
+	System.out.println(x-y);
+	System.out.println(x/y);
+	System.out.println(x%y);
+  }
+}
