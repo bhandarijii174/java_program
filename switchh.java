1f098b3d2d94781bed480fa85188fea40e9b0781
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-24 19:48:52 +0530

    Create switchh.java

---
+++ switchh.java
@@ -0,0 +1,28 @@
+public class switchh {
+  public static void main(String[] args) {
+    int d = 5;
+    switch (d) {
+      case 1:
+        System.out.println("physics");
+        break;
+      case 2:
+        System.out.println("chemistry");
+        break;
+      case 3:
+        System.out.println("mathematics");
+        break;
+      case 4:
+        System.out.println("hindi");
+        break;
+      case 5:
+        System.out.println("politics");
+        break;
+      case 6:
+        System.out.println("english");
+        break;
+      case 7:
+        System.out.println("science");
+        break;
+    }
+  }
+}
