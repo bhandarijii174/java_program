10db49e8af314262c786a94de0353554e9611187
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-28 19:08:30 +0530

    Create attribute.java

---
+++ attribute.java
@@ -0,0 +1,9 @@
+public class attribute {
+  int i;
+
+  public static void main(String[] args) {
+    attribute myObj = new attribute();
+    myObj.i = 56;// value of i is changed to 56.
+    System.out.println(myObj.i);
+  }
+}
