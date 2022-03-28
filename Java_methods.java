dce217c5480a8528d0aa2e29aa917d876c55a15c
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-28 19:30:08 +0530

    Create Java_methods.java

---
+++ Java_methods.java
@@ -0,0 +1,9 @@
+public class Java_methods {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+      }
+    
+      public static void main(String[] args) {
+        myMethod();
+      }
+}
