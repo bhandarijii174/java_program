2d70f21bd5e8107be149dacec0559f3b624185ed
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-28 19:27:33 +0530

    Create Java_inner_class.java

---
+++ Java_inner_class.java
@@ -0,0 +1,16 @@
+public class Java_inner_class {
+    int x = 10;
+
+  class InnerClass {
+    int y = 5;
+  }
+
+
+
+  public static void main(String[] args) {
+    OuterClass myOuter = new OuterClass();
+    OuterClass.InnerClass myInner = myOuter.new InnerClass();
+    System.out.println(myInner.y + myOuter.x);
+  
+}
+}
