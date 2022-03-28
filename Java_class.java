7d63db0e2e21f8a6c74a087550191484a1fa9e1c
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-28 19:24:14 +0530

    Create Java_class.java

---
+++ Java_class.java
@@ -0,0 +1,21 @@
+public class Java_class {
+    class Bicycle {
+
+        // state or field
+        private int gear = 5;
+      
+        // behavior or method
+        public void braking() {
+          System.out.println("Working of Braking");
+        }
+
+        public int getGear() {
+            return gear;
+        }
+
+        public void setGear(int gear) {
+            this.gear = gear;
+        }
+      }
+ 
+}
