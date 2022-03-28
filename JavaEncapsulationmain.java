f515920dd8b0aa5cfddde08b532c80983d38d09e
bhandarijii174 <93576318+bhandarijii174@users.noreply.github.com>
2022-03-28 19:33:59 +0530

    Create JavaEncapsulationmain.java

---
+++ JavaEncapsulationmain.java
@@ -0,0 +1,9 @@
+import java.security.Permission;
+
+public class main {
+    public static void main(String[] args) {
+        Person myObj = new Permission();
+        myObj.name = "John";
+        System.out.println(myObj.name);
+      }
+}
